7ac54b771d5a23b23a0596a0f1731bccfa761498
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-18 23:28:15 +0700

    [FEATURE] Add component 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './auth/login/login.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { NotfoundComponent } from './shared/notfound/notfound.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

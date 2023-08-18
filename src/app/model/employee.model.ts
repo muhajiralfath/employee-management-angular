aa078f7c582685988f2bbd7b0dd9c801e72fc89b
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-19 02:22:47 +0700

    [FEATURE] add Model Employee

---
+++ src/app/model/employee.model.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  'username': string,
+  'firsName': string,
+  'lastName': string,
+  'email': string,
+  'birtDate': Date,
+  'basicSalary': number,
+  'status': string,
+  'group': string,
+  'description': string
+}
